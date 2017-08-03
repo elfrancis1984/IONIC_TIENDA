28f6a1c50575faac446a894c7ac98e2dc0303fac
Francis <francischalan@gmail.com>
2017-08-03 18:12:56 -0500

    correcion

--- src/config/url.servicios.ts
+++ src/config/url.servicios.ts
@@ -1,3 +1,3 @@
 
-export const URL_SERVICIOS = "http://serviciosgidesarrollo/rest/index.php"
+export const URL_SERVICIOS = "http://serviciosgidesarrollo.trabajo.gob.ec/rest/index.php"
 export const URL_IMAGENES = "http://serviciosgidesarrollo.trabajo.gob.ec/rest/public/img/productos"
